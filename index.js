075856a8b75fb956e68db38505fb584a7c06b215
Eilif <eilif.akerjordet@MacBook-Pro.local>
2020-06-19 13:58:09 +0200

    updated data sorting

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const rp = require("request-promise");
 const otcsv = require("objects-to-csv");
 
 // Define the URLS we will be scraping
-const firstURL = "https://proff.no/laglister?i=p22&samplerFilter=true";
-const firstURLViewJson = `${firstURL}&view=json`; 
-const baseURL = "https://proff.no/laglister/" 
+const firstURL = "https://proff.no/laglister?ef=1&et=300&i=p10158&l=Stavanger&l=Oslo&samplerFilter=true";
+const firstURLViewJson = `${firstURL}&view=json`;
+const baseURL = "https://proff.no/laglister/"
 
 const getPageResult = async (URL) => {
   const rawJSON = await rp(URL);
   const parsedResult = JSON.parse(rawJSON);
-  // index[0] = searchResult, index[1] = next page link;
   return [parsedResult.createListSearchResult.resultList, parsedResult.createListSearchResult.pagination];
 };
+
+const sortData = (data) => {
+  const cleanedData = data.map(e => {
+    return {
+      organisationNumber: e.organisationNumber,
+      companyName: e.displayName,
+      postNumber: e.postalAddress.postNumber,
+      postPlace: e.postalAddress.postPlace,
+      revenue: e.revenue || 'not found',
+      numberOfEmployees: e.numberOfEmployees || 'not found',
+      companyAccountsLastUpdatedDate: e.companyAccountsLastUpdatedDate || 'not found',
+      contactPersonRole: typeof e.personRoles[0]  === 'undefined' ? 'not found' : e.personRoles[0].roleName,
+      contactPersonName: typeof e.personRoles[0]  === 'undefined' ? 'not found' : e.personRoles[0].person.name,
+      uri: e.uri
+    };
+  });
+  return cleanedData;
+}
 
 const getDataFromAllPages = async () => {
   const parsedJSON = await getPageResult(firstURLViewJson);
@@ -31,7 +48,8 @@ const getDataFromAllPages = async () => {
       break;
     }
   }
-  const csv = new otcsv(finalResult);
+  const cleanFinalResult = sortData(finalResult);
+  const csv = new otcsv(cleanFinalResult);
   await csv.toDisk('./outputCSV/searchData.csv');
 }
 
